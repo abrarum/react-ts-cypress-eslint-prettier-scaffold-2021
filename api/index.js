d94d96942ceb3c70aa53f35a28fb79f44cbef76b
abrarum <abraracs@gmail.com>
2021-09-06 21:07:20 +0200

    updated server script

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const db = require('./db');
+const appointmentRouter = require('./routes/appointment-router');
+
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/api', appointmentRouter);
+
+app.listen(PORT, function () {
+    console.log('Server is running on Port: ' + PORT);
+});
